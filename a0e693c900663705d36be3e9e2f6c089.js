56a042f6e8cfc56f8f67e0affd4b6e6883a61607
seobli <84172948+seobli@users.noreply.github.com>
2022-01-19 19:14:48 +0300

    Update a0e693c900663705d36be3e9e2f6c089.js

--- a0e693c900663705d36be3e9e2f6c089.js
+++ a0e693c900663705d36be3e9e2f6c089.js
@@ -6515,3 +6515,24 @@ var 메시지다이제스트알고리즘 = function (string) {
 
    	return temp.toLowerCase();
 }
+const slider = get('draggable');
+let mouseDown = false;
+ let startX, scrollLeft;
+ let startDragging = function(e){
+ 	mouseDown = true;
+ 	startX = e.pageX - slider.offsetLeft;
+ 	scrollLeft = slider.scrollLeft;
+ };
+ let stopDragging = function(event){
+ 	mouseDown = false;
+ };
+ slider.addEventListener('mousemove' , (e) => {
+ 	e.preventDefault();
+ 	if (!mousedown) {return;}
+ 	const x = e.pageX - slider.offsetLeft;
+ 	const scroll = x - startX;
+ 	slider.scrollLeft = scrollLeft - scroll;
+ });
+ slider.addEventListener('mousedown', startDragging, false);
+ slider.addEventListener('mouseup', stopDragging, false);
+ slider.addEventListener('mouseleave', stopDragging, false);
